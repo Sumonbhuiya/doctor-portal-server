ba4675373cd9564ddac6c78ffbc8eee1ea7d0860
Sumonbhuiya <sumonbhuiya97@gmail.com>
2021-11-07 23:49:35 +0600

    set time

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function run() {
         //show appointments
         app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             const query = { email: email, date: date };
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
